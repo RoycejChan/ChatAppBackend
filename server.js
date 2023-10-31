78d3b50b3db5f22e4b4d701951e59ffe9311778f
Roycej-Chan <Rouyce23@gmail.com>
2023-10-30 19:13:15 -0700

    changed cors origin

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://roycechatappio.netlify.app/"], // Set the same origin for Socket.IO
+        origin: ["http://localhost:5173", "https://roycechatappio.onrender.app/"], // Set the same origin for Socket.IO
         methods: ["GET", "POST"]
     }
 });
